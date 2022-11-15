a26d569293120901d14ceed9bcd8fe0b3028c610
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-11-15 03:57:00 -0300

    att projeto

--- js/main.js
+++ js/main.js
@@ -1,38 +1,25 @@
-function createGame(player1, hour, player2, details) {
-    return `
-    <p>${details}</p>
-      <li>
-        <img src="/img/icon=${player1}.png" alt="Teste ${player1}" />
-        <strong>${hour}</strong>
-        <img src="./assets/bandeiras-dos-países/icon=${player2}.svg" alt="Bandeira da ${player2}" />
-      </li>
-    `
-}
 
-function createCard(date, day, games) {
-    return `
+//cria cards
+function createCard(date, day, eventName) {
+  return `
        <div class="card">
        <h2>${date}<span>${day}</span></h2>
           <ul>
-            ${games}
+            ${eventName}
           </ul>
         </div>
     `
 }
+//cria informações do card
+function createInfo(imagem, hour, details) {
+  return `
+    <p>${details}</p>
+    <li><img src="/img/icon=${imagem}.jpg" alt="Teste ${imagem}" /></li>
+    <li>${hour}</li>
+      `
+}
 
-
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="/img/ieq-logo-celulas.jpg" alt="Logo da Igreja">
-    </header>
-        <main id="cards">
-        ${createCard("24/11", "quinta", createGame('header-vic', '07:00', 'cameroon', 'Group G')
-    + createGame('brazil', '16:00', 'serbia', '1ª Rodada'))}
-        
-        ${createCard("28/11", "segunda", createGame('cameroon', '07:00', 'serbia', 'Grupo G')
-        + createGame('brazil', '13:00', 'switzerland', '2ª Rodada'))}
-
-        ${createCard("02/12", "sexta", createGame('switzerland', '16:00', 'serbia', 'Grupo G')
-            + createGame('cameroon', '16:00', 'brazil', '3ª Rodada'))}
-        </main>
-`
+document.querySelector("#cards").innerHTML = createCard("19/11", "sábado", "Culto de casais - Noite Italiana" +
+                                            createInfo("massa", "Horário: 19:30", "Igreja Local"))
+                                          + createCard("26/11", "sábado", "Festa da multiplicação" +
+                                            createInfo("festa", "Horário: 19:30", "Igreja"))
