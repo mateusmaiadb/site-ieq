f90ead470170adb43210da34c4c595ec14b479cb
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-11-16 15:12:12 -0300

    meus testes

---
+++ js/pages.js
@@ -0,0 +1,24 @@
+
+//cria cards
+function createCard(date, day, eventName) {
+    return `
+         <div class="card">
+         <h2>${date}<span>${day}</span></h2>
+            <ul>
+              ${eventName}
+            </ul>
+          </div>
+      `
+  }
+  //cria informações do card
+  function createInfo(imagem, hour, details) {
+    return `
+      <p>${details}</p>
+      <li><img src="/img/icon=${imagem}.jpg" alt="Teste ${imagem}" /></li>
+      <p>${hour}</p>
+        `
+  }
+  
+  
+document.querySelector("#cards").innerHTML = createCard("19/11", "sábado", "Culto de casais - Noite Italiana" +
+                                              createInfo("massa", "Horário: 19:30", "Igreja Local"))                                            
